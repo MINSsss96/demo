0b8248f9d47c804b9a7eced9eb1be0c6f592320c
MINSsss96 <minsue0422@naver.com>
2025-08-06 18:07:37 +0900

    구구단 과제중

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 
@@ -12,7 +18,12 @@ public class TestController {
         return "main";
     }
     @GetMapping("/gugu")
-    public String printGugu(){
+    public String printGugu(Model model){
+        List<Integer> danList = Arrays.asList(2, 3, 9);
+        List<Integer> numList = IntStream.rangeClosed(1, 9).boxed().collect(Collectors.toList());
+
+        model.addAttribute("danList", danList);
+        model.addAttribute("numList", numList);
         return "/gugudan/gugu";
     }
 }
